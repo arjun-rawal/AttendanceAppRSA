a1717cc8a7c0dfaa6cdb692c4620143739885a7b
Sen-26 <81495960+Sen-26@users.noreply.github.com>
2025-03-17 18:49:49 -0400

    fixed content screen
    added classbar and  fixed content loading error

--- screens/ContentScreen.jsx
+++ screens/ContentScreen.jsx
@@ -3,24 +3,30 @@ import React, { useState, useEffect } from 'react';
 import { SafeAreaView, ScrollView, Text, StyleSheet, ActivityIndicator } from 'react-native';
 import { doc, getDoc } from 'firebase/firestore';
 import { db } from '../firebaseConfig';
-
-export default function ContentScreen({ route }) {
+import ClassBar from '../components/ClassBar';
+export default function ContentScreen({ route, navigation }) {
   // Expect route.params to include { className, selectedDate }
-  const { className, selectedDate } = route.params;
+  const { name, date } = route.params.item;
+  const className = name;
+  const selectedDate = date;
   const [content, setContent] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchContent = async () => {
       try {
+        console.log(route.params);
         const docRef = doc(db, "classes", className, "lessons", selectedDate);
+        
         const docSnap = await getDoc(docRef);
+        
         if (docSnap.exists()) {
           setContent(docSnap.data().content);
         } else {
           setContent("No content available for this class on this day.");
         }
       } catch (error) {
+        console.error(error);
         setContent("Error loading content.");
       } finally {
         setLoading(false);
@@ -30,7 +36,7 @@ export default function ContentScreen({ route }) {
   }, [className, selectedDate]);
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.container} edges={["left","right"]}>
       <ScrollView contentContainerStyle={styles.contentContainer}>
         {loading ? (
           <ActivityIndicator size="large" color="#f4511e" />
@@ -38,6 +44,11 @@ export default function ContentScreen({ route }) {
           <Text style={styles.contentText}>{content}</Text>
         )}
       </ScrollView>
+      <ClassBar
+        initialIndex={1}
+        navigation={navigation}
+        selectedInfo={route.params}
+      />
     </SafeAreaView>
   );
 }
